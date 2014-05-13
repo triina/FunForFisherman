509d953f544436ae20f1960a53e0e74686d25c57
eugene-doe <dobryanskiy@mail.ru>
2014-05-14 01:24:17 +0300

    jquery.validate.fix renamed jquery.fix, included in jqueryval bundle (IE compatibility fix)

--- ForTheFisherman/App_Start/BundleConfig.cs
+++ ForTheFisherman/App_Start/BundleConfig.cs
@@ -16,7 +16,8 @@ namespace ForTheFisherman
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jquery.fix*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
